027559f5d03e550a5bc25a4a5a3355ef62209d55
Syed Ali Shabi <germanali007@gmail.com>
2021-08-22 16:27:46 +0530

    Added installation requirment for petergate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_13_182601) do
+ActiveRecord::Schema.define(version: 2021_08_22_105454) do
 
   create_table "blogs", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
